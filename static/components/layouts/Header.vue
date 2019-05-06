7f989579fb0ef5c3dadf905b7bbffe4c34bab99c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-06 20:46:03 +0900

    using vue-router

--- static/components/layouts/Header.vue
+++ static/components/layouts/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="/">C++ GAE/FE Sample</a>
+        <router-link class="navbar-brand" to="/">C++ GAE/FE Sample</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,9 +11,9 @@
                     Menu
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Top</a>
-                    <a class="dropdown-item" href="#">About</a>
-                    <a class="dropdown-item" href="#">Contact</a>
+                    <router-link class="dropdown-item" to="/">Top</router-link>
+                    <router-link class="dropdown-item" to="/about">About</router-link>
+                    <router-link class="dropdown-item" to="/contact">Contact</router-link>
                   </div>
                 </li>
             </ul>
--- static/src/index.js
+++ static/src/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
+import Router from '../router/index';
+
 Vue.use(Bootstrap);
 
 import Header from '../components/layouts/Header.vue';
 
 const app = new Vue({
     el: ".app",
+    router: Router,
     components: {
         'nav-bar': Header
     }
