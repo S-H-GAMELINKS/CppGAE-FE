f217398b0b832bb43a18bb2a50eb19ad4f74190a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-06 18:53:51 +0900

    using static/index.html

--- main.cpp
+++ main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "cpp-httplib/httplib.h"
+
+const std::string load_static(const std::string& path) {
+
+    std::ifstream static_file(path.c_str(), std::ios::in);
+
+    std::stringstream stream;
+
+    stream << static_file.rdbuf();
+    static_file.close();
+
+    return stream.str();
+}
 
 int main() {
     std::cout << "C++ GAE/FE is up!" << std::endl;
 
     httplib::Server svr;
 
+    const std::string html = load_static("static/index.html"); 
+
     svr.Get("/", [&](const httplib::Request& req, httplib::Response& res){
         std::cout << "GET: routes '/'" << std::endl;
-        res.set_content("Hello World!", "text/plain");
+        res.set_content(html, "text/html");
     });
 
     svr.listen("0.0.0.0", 8080);
