0a66293b2472ac0a7bfbe12dbdd333f3a716c12c
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-06 19:53:13 +0900

    add Header.vue

---
+++ static/components/layouts/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="/">ロクス ソラス</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    メニュー
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">トップ</a>
+                    <a class="dropdown-item" href="#">サービス概要</a>
+                    <a class="dropdown-item" href="#">問い合わせ</a>
+                  </div>
+                </li>
+            </ul>
+        </div>
+    </nav>    
+</template>
