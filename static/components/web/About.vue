ad67c98ffa73341070c27a396af2c00b993b65b4
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-07 17:06:13 +0900

    using jumbotron for about

--- static/components/web/About.vue
+++ static/components/web/About.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="container">
-        <h1>About Pages</h1>
+        <div class="jumbotron">
+            <h1 class="display-4">About</h1>
+            <p class="lead">Very Simple Chat App!</p>
+            <hr class="my-4">
+            <p>Developed by C++/Stmilus/Vue.js/FireBase/Docker</p>
+        </div>
     </div>    
 </template>
