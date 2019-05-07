cacfdd7983a8b8def799b0b397a29b798c642cca
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-07 11:15:36 +0900

    add store/index.js

---
+++ static/store/index.js
@@ -0,0 +1,62 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
+import FireBase from 'firebase';
+
+Vue.use(Vuex);
+
+const firebase = FireBase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DB_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGEING_SENDER_ID
+});
+
+const database = firebase.database();
+
+const provider = new FireBase.auth.GithubAuthProvider();
+
+const store = new Vuex.Store({
+    state: {
+        firebase: firebase,
+        database: database,
+        id: "",
+        name: "",
+        session: false,
+        provider: provider
+    },
+    mutations: {
+        loginLocusSolus(state, id) {
+            state.id = String(id);
+        },
+        setUserName(state, name) {
+            state.name = String(name);
+        },
+        getSession(state, bool){
+            state.session = bool;
+        }
+    },
+    plugins: [ createPersistedState(
+        {
+            key: 'loginSession',
+            paths: ['session'],
+            storage: window.sessionStorage,
+        }),
+        createPersistedState(
+        {
+            key: 'loginId',
+            paths: ['id'],
+            storage: window.sessionStorage,
+        }),
+        createPersistedState(
+        {
+            key: 'loginName',
+            paths: ['name'],
+            storage: window.sessionStorage,
+        }
+    )]
+});
+
+export default store;
